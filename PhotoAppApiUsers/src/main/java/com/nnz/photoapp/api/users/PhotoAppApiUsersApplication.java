08d67a447489f2b2e303d05b456d082ad8d89569
NyiNyiZin <nyinyizin1148@gmail.com>
2020-02-23 15:46:50 +0630

    add refresh scope in apiUser

--- PhotoAppApiUsers/src/main/java/com/nnz/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/nnz/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -3,11 +3,13 @@ package com.nnz.photoapp.api.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@RefreshScope
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
